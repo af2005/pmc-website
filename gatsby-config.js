2c9e95c7221330e7c45de9c2459f6a33c8db3350
Alexander Franke <a.franke@posteo.de>
2022-06-10 15:40:47 +0200

    added pre-commit hook for linting

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `PMC website`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://test.powermindclub.com`,
   },
   plugins: [
     //   {
