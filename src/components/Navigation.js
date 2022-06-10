fefef4af48f36eb66b695b037a74f55dad5e0519
Alexander Franke <a.franke@posteo.de>
2022-06-10 18:14:38 +0200

    zeitleiste in about-us; nav active item

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,6 +4,21 @@ import { Navbar, Button } from "flowbite-react";
 import logo from "../img/logo/min/logo-black.svg";
 import { Link } from "gatsby";
 
+function NavbarLink(props) {
+  return (
+    <li>
+      <Link to={props.to}
+            className="nav-link hover:cursor-pointer block py-2 pr-4 pl-3 md:p-0 md:py-2 border-b border-gray-100
+          text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400
+          dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent
+          md:hover:text-black md:dark:hover:bg-transparent md:dark:hover:text-white"
+            activeClassName="nav-link-active md:[border-bottom-width:4px]">
+        {props.children}
+      </Link>
+    </li>
+  );
+}
+
 export function Navigation() {
   return (
     <>
@@ -19,11 +34,13 @@ export function Navigation() {
           <Navbar.Toggle />
         </div>
         <Navbar.Collapse>
-          <Navbar.Link href="/">Home</Navbar.Link>
-          <Navbar.Link href="/about-us">Über uns</Navbar.Link>
-          <Navbar.Link href="/why">Warum PMC</Navbar.Link>
-          <Navbar.Link href="/app">App</Navbar.Link>
-          <Navbar.Link href="/blog">Blog</Navbar.Link>
+            <NavbarLink to={"/"}>
+              Home
+            </NavbarLink>
+          <NavbarLink to="/about-us">Über uns</NavbarLink>
+          <NavbarLink to="/why" >Warum PMC</NavbarLink>
+          <NavbarLink to="/app" >App</NavbarLink>
+          <NavbarLink to="/blog">Blog</NavbarLink>
         </Navbar.Collapse>
       </Navbar>
     </>
